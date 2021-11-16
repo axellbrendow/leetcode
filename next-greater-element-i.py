bbe0f88c7b7a468fc13e80bc3940c7835cbd5aa0
axell-brendow <breno.axel@gmail.com>
2021-11-16 12:58:25 -0300

    Change formatting

--- next-greater-element-i.py
+++ next-greater-element-i.py
@@ -6,7 +6,10 @@ def find_next_greater_elements(nums1, nums2):  # n2 [1, 2, -3, 4, -5, 6], n1 [6,
 		while len(stack) > 0 and num > nums2[stack[-1]]:
 			greater[stack.pop()] = i
 		stack.append(i)
-	return [nums2[greater[pos[nums1[i]]]] if greater[pos[nums1[i]]] != -1 else -1 for i in range(len(nums1))]  # i = 2, [ -1, 3, 5 ]
+	return [
+		nums2[greater[pos[nums1[i]]]] if greater[pos[nums1[i]]] != -1 else -1 \
+		for i in range(len(nums1))
+	]  # i = 2, [ -1, 3, 5 ]
 
 assert str(find_next_greater_elements([2,4], [1,2,3,4])) == str([3,-1])
 assert str(find_next_greater_elements([4,1,2], [1,3,4,2])) == str([-1,3,-1])
