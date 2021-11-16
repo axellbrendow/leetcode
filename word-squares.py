1804882ab4c1c045a5eb02432bc21bac304f7fe7
axell-brendow <breno.axel@gmail.com>
2021-11-16 12:58:46 -0300

    Add "Word Squares"

---
+++ word-squares.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+
+def create_hashmap_of_prefixes_and_words(words_set):
+	hashmap = defaultdict(list)
+	for word in words_set:
+		for i in range(len(word) + 1):
+			hashmap[word[:i]].append(word)
+	return hashmap
+
+def get_words_length(words_set):
+	for word in words_set:
+		return len(word)
+
+def find_all_word_squares(words_set):
+	nlines = get_words_length(words_set)
+	hashmap = create_hashmap_of_prefixes_and_words(words_set)
+	word_squares = []
+	word_square = []
+	
+	def dfs(prefix, line):
+		if line == nlines:
+			word_squares.append(word_square.copy())
+			return True
+		words = hashmap[prefix]
+		for word in words:
+			word_square.append(word)
+			new_prefix = ''.join([
+				wrd[line + 1] if line + 1 < nlines else 'x'
+				for wrd in word_square]
+			)
+			dfs(new_prefix, line + 1)
+			word_square.pop()
+
+	dfs('', 0)
+	return word_squares
+
+words_set = ["area","lead","wall","lady","ball"]
+output = [
+  [ "wall",
+    "area",
+    "lead",
+    "lady"
+  ],
+  [ "ball",
+    "area",
+    "lead",
+    "lady"
+  ]
+]
+assert str(find_all_word_squares(words_set)) == str(output)
+
+words_set = ["abat","baba","atan","atal"]
+output = [
+  [ "baba",
+    "abat",
+    "baba",
+    "atan"
+  ],
+  [ "baba",
+    "abat",
+    "baba",
+    "atal"
+  ]
+]
+assert str(find_all_word_squares(words_set)) == str(output)
