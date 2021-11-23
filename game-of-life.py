eb401f532fcdca1269d72057cc6066b61b286f73
axell-brendow <breno.axel@gmail.com>
2021-11-22 22:08:14 -0300

    Improve "Game of Life"

--- game-of-life.py
+++ game-of-life.py
@@ -15,25 +15,14 @@ class GameOfLife:
 		]
 		self.create_live_neighbors()
 
-	def out_of_bounds(self, i, j):
-		return i < 0 or i >= self.nlines or j < 0 or j >= self.ncolumns
-
-	def get(self, i, j):
-		return 0 if self.out_of_bounds(i, j) else self.live_neighbors[i][j]
-
-	def update(self, i, j, new_value):
-		if self.out_of_bounds(i, j): return
-		self.live_neighbors[i][j] = new_value
-
 	def update_neighbors(self, i, j, delta):
 		for direction in self.directions:
 			new_i = i + direction[0]
 			new_j = j + direction[1]
-			new_value = self.get(new_i, new_j) + delta
-			self.update(new_i, new_j, new_value)
+			self.live_neighbors[new_i][new_j] += delta
 
 	def create_live_neighbors(self):
-		self.live_neighbors = [[0] * self.ncolumns for _ in range(self.nlines)]
+		self.live_neighbors = [[0] * (self.ncolumns + 1) for _ in range(self.nlines + 1)]
 		for i in range(self.nlines):
 			for j in range(self.ncolumns):
 				if self.board[i][j] == 1:
