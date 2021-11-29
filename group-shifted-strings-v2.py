33958d88fb0f11d392a19817bce0f5ffd2f07b0e
axell-brendow <breno.axel@gmail.com>
2021-11-29 10:17:48 -0300

    Do not override built-in hash function

--- group-shifted-strings-v2.py
+++ group-shifted-strings-v2.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
 
-def hash(string):
+def hash_str(string):
 	return tuple([(ord(c) - ord(string[0])) % 26 for c in string])
 
 def group_shifted_strings(strings):  # ['', 'a', '', 'b', 'az', 'ba']
 	buckets = defaultdict(list)
 	for string in strings:
-		buckets[hash(string)].append(string)
+		buckets[hash_str(string)].append(string)
 	return list(buckets.values())
 
 strings = ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"]
