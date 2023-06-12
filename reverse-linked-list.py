b3a046d25922a2dabf1148acd522f70d75085679
axell-brendow <breno.axel@gmail.com>
2023-06-12 16:40:03 -0300

    Add "Reverse Linked List" V1 in Python

---
+++ reverse-linked-list.py
@@ -0,0 +1,62 @@
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, ListNode):
+            return False
+
+        me = self
+        them = __value
+
+        while me and them:
+            if me.val != them.val:
+                return False
+            me = me.next
+            them = them.next
+
+        return not me and not them
+
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head:
+        return None
+
+    first_node = head
+    last_node = head
+
+    while last_node.next:
+        last_node_next_next = last_node.next.next
+
+        last_node.next.next = first_node
+        first_node = last_node.next
+
+        last_node.next = last_node_next_next
+
+    return first_node
+
+
+head = None
+expected_output = None
+output = reverseList(head)
+assert output == expected_output
+
+head = ListNode(5)
+expected_output = ListNode(5)
+output = reverseList(head)
+assert output == expected_output
+
+head = ListNode(5, ListNode(7))
+expected_output = ListNode(7, ListNode(5))
+output = reverseList(head)
+assert output == expected_output
+
+head = ListNode(5, ListNode(7, ListNode(-1)))
+expected_output = ListNode(-1, ListNode(7, ListNode(5)))
+output = reverseList(head)
+assert output == expected_output
