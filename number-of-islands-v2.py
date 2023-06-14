e0590631356df6e5224ce0c183698b10ce87d2b9
axell-brendow <breno.axel@gmail.com>
2023-06-13 22:29:09 -0300

    Updating some variable names

--- number-of-islands-v2.py
+++ number-of-islands-v2.py
@@ -2,7 +2,7 @@ class Solution(object):
     def numIslands(self, grid):
         nlines = len(grid)
         ncolumns = len(grid[0])
-        num_of_islands = sum([grid[i][j] == '1' for i in range(nlines) for j in range(ncolumns)])
+        num_of_islands = 0
         group_leader = [i for i in range(nlines * ncolumns)]
 
         def find_group_leader(cell):
@@ -10,25 +10,27 @@ class Solution(object):
                 cell = group_leader[cell]
             return cell
 
-        def union(cell1, cell2):
+        def union(curr_cell, prev_cell):
             nonlocal num_of_islands
-            group_leader1 = find_group_leader(cell1)
-            group_leader2 = find_group_leader(cell2)
-            if group_leader1 != group_leader2:
+            curr_cell_leader = find_group_leader(curr_cell)
+            prev_cell_leader = find_group_leader(prev_cell)
+            if curr_cell_leader != prev_cell_leader:
                 # All subordinated cells will have a new leader
-                group_leader[group_leader1] = group_leader2
+                group_leader[curr_cell_leader] = prev_cell_leader
                 num_of_islands -= 1
 
         for i in range(nlines):
             for j in range(ncolumns):
                 if grid[i][j] == '1':
+                    num_of_islands += 1
                     if i - 1 >= 0 and grid[i - 1][j] == '1':
-                        union(i * ncolumns + j, i * ncolumns + j - ncolumns)
-                    
+                        union(i * ncolumns + j, (i - 1) * ncolumns + j)
+
                     if j - 1 >= 0 and grid[i][j - 1] == '1':
-                        union(i * ncolumns + j, i * ncolumns + j - 1)
+                        union(i * ncolumns + j, i * ncolumns + (j - 1))
 
         return num_of_islands
+
 
 assert Solution().numIslands([
     ['0', '1', '0', '0'],
@@ -38,14 +40,14 @@ assert Solution().numIslands([
 ]) == 2
 
 assert Solution().numIslands([
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
 ]) == 1
 
 assert Solution().numIslands([
-    ["0","1","0"],
-    ["1","0","1"],
-    ["0","1","0"]
+    ["0", "1", "0"],
+    ["1", "0", "1"],
+    ["0", "1", "0"]
 ]) == 4
