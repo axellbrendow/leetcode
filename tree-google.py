255bda4048718516b9ec508a5a35074e425a5f14
axell-brendow <breno.axel@gmail.com>
2024-07-19 11:43:31 -0300

    Rename variables

--- tree-google.py
+++ tree-google.py
@@ -9,20 +9,20 @@
 #        G       H        B
 #     W               C       D
 
-def remove_one_node(tree, index):
+def remove_node(tree, node_index):
     for i in range(len(tree)):
-        if tree[i][1] > index:
+        if tree[i][1] > node_index:
             tree[i] = (tree[i][0], tree[i][1] - 1)
-        elif tree[i][1] == index:
-            tree[i] = (tree[i][0], tree[index][1])
-    tree.pop(index)
+        elif tree[i][1] == node_index:
+            tree[i] = (tree[i][0], tree[node_index][1])
+    tree.pop(node_index)
 
 
 tree = [('A', 0), ('F', 0), ('B', 0), ('G', 1),
         ('H', 1), ('C', 2), ('D', 2), ('W', 3)]
-index = 1
+node_index = 1
 output = [('A', 0), ('B', 0), ('G', 0), ('H', 0), ('C', 1), ('D', 1), ('W', 2)]
-remove_one_node(tree, index)
+remove_node(tree, node_index)
 assert tree == output
 
 
@@ -39,13 +39,13 @@ def remove_subtree(tree, root_index):
         while child != -1:
             dfs(child)
             child = get_first_child(tree, node)
-        remove_one_node(tree, node)
+        remove_node(tree, node)
     dfs(root_index)
 
 
 tree = [('A', 0), ('F', 0), ('B', 0), ('G', 1),
         ('H', 1), ('C', 2), ('D', 2), ('W', 3)]
-root_index = 1
+node_index = 1
 output = [('A', 0), ('B', 0), ('C', 1), ('D', 1)]
-remove_subtree(tree, root_index)
+remove_subtree(tree, node_index)
 assert tree == output
