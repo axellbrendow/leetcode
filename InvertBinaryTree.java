fbb0f80910f5a6693ad7995258112ba296b231f9
axell-brendow <breno.axel@gmail.com>
2024-07-24 10:52:41 -0300

    Add "Invert Binary Tree" explanation

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -1,5 +1,7 @@
 /*-
 https://leetcode.com/problems/invert-binary-tree/description/
+
+If you rotate a tree using the root as the axis, everything that is on the right side will go to the left side
 */
 
 public class InvertBinaryTree {
@@ -17,16 +19,19 @@ public class InvertBinaryTree {
     assert invertBinaryTree(null) == null;
     assert invertBinaryTree(new TreeNode()).equals(new TreeNode());
     assert !invertBinaryTree(new TreeNode()).equals(new TreeNode(1));
+
     assert invertBinaryTree(
       new TreeNode(0, new TreeNode(), null)
     ).equals(
       new TreeNode(0, null, new TreeNode())
     );
+
     assert invertBinaryTree(
       new TreeNode(0, null, new TreeNode())
     ).equals(
       new TreeNode(0, new TreeNode(), null)
     );
+
     assert invertBinaryTree(
       new TreeNode(0, new TreeNode(1), new TreeNode(2))
     ).equals(
