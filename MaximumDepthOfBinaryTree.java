c239e3ee4a04c8cf0c32b0679aafa6ab47b84bb3
axell-brendow <breno.axel@gmail.com>
2024-07-24 00:09:55 -0300

    Add "Maximum Depth of Binary Tree" in Java

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,41 @@
+/*-
+https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+*/
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+public class MaximumDepthOfBinaryTree {
+  public static int maxDepth(TreeNode root, int depth) {
+    if (root == null) return depth;
+    return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1));
+  }
+
+  public static int maxDepth(TreeNode root) {
+    return maxDepth(root, 0);
+  }
+
+  public static void main(String[] args) {
+    assert maxDepth(null) == 0;
+    assert maxDepth(new TreeNode()) == 1;
+    assert maxDepth(new TreeNode(0, new TreeNode(), null)) == 2;
+    assert maxDepth(new TreeNode(0, null, new TreeNode())) == 2;
+    assert maxDepth(new TreeNode(0, new TreeNode(), new TreeNode())) == 2;
+  }
+}
