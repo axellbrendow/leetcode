baf0319979da184a9e825fe819361ba46326beb5
axell-brendow <breno.axel@gmail.com>
2024-07-26 21:40:45 -0300

    Add fibonacci sequence recurrence relation proof

--- TwoSumV2.java
+++ TwoSumV2.java
@@ -5,16 +5,19 @@ import java.util.stream.IntStream;
 class TwoSumV2 {
     public int[] twoSum(int[] nums, int target) {
         final var numToIndices = IntStream.range(0, nums.length).boxed()
-                .collect(Collectors.groupingBy(index -> nums[index]));
+            .collect(Collectors.groupingBy(index -> nums[index]));
 
         return Arrays.stream(nums)
-                .filter(num -> num + num == target ? numToIndices.get(num).size() > 1
-                        : numToIndices.containsKey(target - num))
-                .mapToObj(num -> new int[] {
-                        numToIndices.get(num).get(0),
-                        numToIndices.get(target - num).get(num + num == target ? 1 : 0)
-                })
-                .findFirst().get();
+            .filter(
+                num -> num + num == target
+                    ? numToIndices.get(num).size() > 1
+                    : numToIndices.containsKey(target - num)
+            )
+            .mapToObj(num -> new int[]{
+                numToIndices.get(num).get(0),
+                numToIndices.get(target - num).get(num + num == target ? 1 : 0)
+            })
+            .findFirst().get();
     }
 
     public static void main(String[] args) {
@@ -22,23 +25,23 @@ class TwoSumV2 {
 
         var testNumber = 0;
 
-        var nums = new int[] { 2, 7, 11, 15 };
+        var nums = new int[]{2, 7, 11, 15};
         var target = 9;
-        var expectedOutput = new int[] { 0, 1 };
+        var expectedOutput = new int[]{0, 1};
         var output = twoSum.twoSum(nums, target);
         var testPassed = Arrays.equals(expectedOutput, output);
         System.out.println("Test " + ++testNumber + (testPassed ? " succeeded" : " failed"));
 
-        nums = new int[] { 3, 2, 4 };
+        nums = new int[]{3, 2, 4};
         target = 6;
-        expectedOutput = new int[] { 1, 2 };
+        expectedOutput = new int[]{1, 2};
         output = twoSum.twoSum(nums, target);
         testPassed = Arrays.equals(expectedOutput, output);
         System.out.println("Test " + ++testNumber + (testPassed ? " succeeded" : " failed"));
 
-        nums = new int[] { 3, 3 };
+        nums = new int[]{3, 3};
         target = 6;
-        expectedOutput = new int[] { 0, 1 };
+        expectedOutput = new int[]{0, 1};
         output = twoSum.twoSum(nums, target);
         testPassed = Arrays.equals(expectedOutput, output);
         System.out.println("Test " + ++testNumber + (testPassed ? " succeeded" : " failed"));
--- TwoSumV3.java
+++ TwoSumV3.java
@@ -7,14 +7,16 @@ class TwoSumV3 {
         final var numToIndices = new HashMap<Integer, Integer>();
 
         return IntStream.range(0, nums.length)
-                .filter(index -> {
-                    final var complementPresent = numToIndices.containsKey(target - nums[index]);
-                    if (!complementPresent)
-                        numToIndices.put(nums[index], index);
-                    return complementPresent;
-                })
-                .mapToObj(index -> new int[] { numToIndices.get(target - nums[index]), index })
-                .findFirst().get();
+            .filter(index -> {
+                final var complementPresent = numToIndices.containsKey(target - nums[index]);
+                if (!complementPresent)
+                    numToIndices.put(nums[index], index);
+                return complementPresent;
+            })
+            .mapToObj(index -> new int[]{
+                numToIndices.get(target - nums[index]), index
+            })
+            .findFirst().get();
     }
 
     public static void main(String[] args) {
@@ -22,23 +24,23 @@ class TwoSumV3 {
 
         var testNumber = 0;
 
-        var nums = new int[] { 2, 7, 11, 15 };
+        var nums = new int[]{2, 7, 11, 15};
         var target = 9;
-        var expectedOutput = new int[] { 0, 1 };
+        var expectedOutput = new int[]{0, 1};
         var output = twoSum.twoSum(nums, target);
         var testPassed = Arrays.equals(expectedOutput, output);
         System.out.println("Test " + ++testNumber + (testPassed ? " succeeded" : " failed"));
 
-        nums = new int[] { 3, 2, 4 };
+        nums = new int[]{3, 2, 4};
         target = 6;
-        expectedOutput = new int[] { 1, 2 };
+        expectedOutput = new int[]{1, 2};
         output = twoSum.twoSum(nums, target);
         testPassed = Arrays.equals(expectedOutput, output);
         System.out.println("Test " + ++testNumber + (testPassed ? " succeeded" : " failed"));
 
-        nums = new int[] { 3, 3 };
+        nums = new int[]{3, 3};
         target = 6;
-        expectedOutput = new int[] { 0, 1 };
+        expectedOutput = new int[]{0, 1};
         output = twoSum.twoSum(nums, target);
         testPassed = Arrays.equals(expectedOutput, output);
         System.out.println("Test " + ++testNumber + (testPassed ? " succeeded" : " failed"));
