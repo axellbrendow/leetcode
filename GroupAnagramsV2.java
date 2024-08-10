f87ba4dffbedd9137ff2017d96338bbe95700375
axell-brendow <breno.axel@gmail.com>
2024-08-10 01:17:05 -0300

    Add "Longest Consecutive Sequence" in Java

--- GroupAnagramsV2.java
+++ GroupAnagramsV2.java
@@ -6,7 +6,8 @@ https://leetcode.com/problems/group-anagrams/description/
 
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically
+using all the original letters exactly once.
 
 1 <= strs.length <= 10^4
 0 <= strs[i].length <= 100
