81ffb87b924e4e27f1db99bcee1dc912aea4e0ce
axell-brendow <breno.axel@gmail.com>
2024-09-01 17:36:25 -0300

    Update LongestPalindromicSubstring

--- java/myjava/LongestPalindromicSubstringV1.java
+++ java/myjava/LongestPalindromicSubstringV1.java
@@ -2,30 +2,39 @@ package myjava;
 
 // TIME LIMIT EXCEEDED
 class LongestPalindromicSubstringV1 {
-    public String longestPalindrome(String s) {
-        // s = "", output = ""
-        // s = "a", output = "a"
-        // s = "ab", output = "a"
-        // s = "abc", output = "a"
-        // s = "abb", output = "bb"
-        var longestPalindrome = "";
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                final var substr = s.substring(i, j);
-                if (isPalindrome(substr) && substr.length() > longestPalindrome.length()) {
-                    longestPalindrome = substr;
-                }
-            }
+  public static String longestPalindrome(String s) {
+    // s = "", output = ""
+    // s = "a", output = "a"
+    // s = "ab", output = "a"
+    // s = "abc", output = "a"
+    // s = "abb", output = "bb"
+    var longestPalindrome = "";
+    for (int i = 0; i < s.length(); i++) {
+      for (int j = i + 1; j <= s.length(); j++) {
+        final var substr = s.substring(i, j);
+        if (isPalindrome(substr) && substr.length() > longestPalindrome.length()) {
+          longestPalindrome = substr;
         }
-        return longestPalindrome;
+      }
     }
+    return longestPalindrome;
+  }
 
-    private boolean isPalindrome(String s) {
-        for (int i = 0, j = s.length() - 1; i <= j; i++, j--) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-        }
-        return true;
+  private static boolean isPalindrome(String s) {
+    for (int i = 0, j = s.length() - 1; i <= j; i++, j--) {
+      if (s.charAt(i) != s.charAt(j)) {
+        return false;
+      }
     }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    assert longestPalindrome("").equals("");
+    assert longestPalindrome("a").equals("a");
+    assert longestPalindrome("ab").equals("a");
+    assert longestPalindrome("abc").equals("a");
+    assert longestPalindrome("abb").equals("bb");
+    assert longestPalindrome("bab").equals("bab");
+  }
 }
--- java/myjava/LongestPalindromicSubstringV2.java
+++ java/myjava/LongestPalindromicSubstringV2.java
@@ -1,38 +1,32 @@
 package myjava;
 
 class LongestPalindromicSubstringV2 {
-    public static String longestPalindrome(String s) {
-        final var isPalindromeBetween = new boolean[s.length()][s.length()];
-        var longestPalindrome = "";
+  public static String longestPalindrome(String s) {
+    final var isPalindromeBetween = new boolean[s.length()][s.length()];
+    var longestPalindrome = "";
 
-        for (int currLength = 1; currLength <= isPalindromeBetween.length; currLength++) {
-            for (int start = 0; start < isPalindromeBetween.length - currLength + 1; start++) {
-                final var end = start + currLength - 1;
-                isPalindromeBetween[start][end] = s.charAt(start) == s.charAt(end) && (currLength == 1 ||
-                    currLength == 2 || isPalindromeBetween[start + 1][end - 1]);
+    for (int currLength = 1; currLength <= isPalindromeBetween.length; currLength++) {
+      for (int start = 0; start < isPalindromeBetween.length - currLength + 1; start++) {
+        final var end = start + currLength - 1;
+        isPalindromeBetween[start][end] = s.charAt(start) == s.charAt(end) && (currLength == 1 ||
+          currLength == 2 ||
+          isPalindromeBetween[start + 1][end - 1]);
 
-                if (isPalindromeBetween[start][end] && currLength > longestPalindrome.length()) {
-                    longestPalindrome = s.substring(start, end + 1);
-                }
-            }
+        if (isPalindromeBetween[start][end] && currLength > longestPalindrome.length()) {
+          longestPalindrome = s.substring(start, end + 1);
         }
-
-        return longestPalindrome;
+      }
     }
 
-    public static void main(String[] args) {
-        var testNumber = 0;
+    return longestPalindrome;
+  }
 
-        var input = "babad";
-        var expectedOutpupt = "bab";
-        var output = longestPalindrome(input);
-        var testPassed = output.equals(expectedOutpupt);
-        System.out.println("Test " + ++testNumber + (testPassed ? " succeeded" : " failed"));
-
-        input = "cbbd";
-        expectedOutpupt = "bb";
-        output = longestPalindrome(input);
-        testPassed = output.equals(expectedOutpupt);
-        System.out.println("Test " + ++testNumber + (testPassed ? " succeeded" : " failed"));
-    }
+  public static void main(String[] args) {
+    assert longestPalindrome("").equals("");
+    assert longestPalindrome("a").equals("a");
+    assert longestPalindrome("ab").equals("a");
+    assert longestPalindrome("abc").equals("a");
+    assert longestPalindrome("abb").equals("bb");
+    assert longestPalindrome("bab").equals("bab");
+  }
 }
