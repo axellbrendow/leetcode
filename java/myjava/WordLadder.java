1ecf2e43d867fc8c497d6f7456b49cdaa6f6370f
axell-brendow <breno.axel@gmail.com>
2024-09-01 11:09:00 -0300

    Simplify WordLadder.java

--- java/myjava/WordLadder.java
+++ java/myjava/WordLadder.java
@@ -3,22 +3,42 @@ package myjava;
 import java.util.*;
 import java.util.stream.*;
 
-public class WordLadder {
-  private static boolean oneDiff(Map<String, Set<Integer>> wordChars, String str1, String str2) {
-    int lengthDiff = Math.abs(str1.length() - str2.length());
-    if (lengthDiff > 1) return false;
-    if (lengthDiff == 1) {
-      String longestStr, smallestStr;
-      if (str1.length() <= str2.length()) {
-        smallestStr = str1;
-        longestStr = str2;
-      } else {
-        smallestStr = str2;
-        longestStr = str1;
-      }
-      return longestStr.contains(smallestStr);
-    }
+/*-
+https://leetcode.com/problems/word-ladder/description/
 
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of
+words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+Every adjacent pair of words differs by a single letter.
+Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+sk == endWord
+
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest
+transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+Example 1:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+
+Example 2:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+Output: 0
+Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+
+Constraints:
+
+1 <= beginWord.length <= 10
+endWord.length == beginWord.length
+1 <= wordList.length <= 5000
+wordList[i].length == beginWord.length
+beginWord, endWord, and wordList[i] consist of lowercase English letters.
+beginWord != endWord
+All the words in wordList are unique.
+*/
+
+public class WordLadder {
+  private static boolean oneDiff(String str1, String str2) {
     int diff = 0;
     for (int i = 0; i < str1.length(); i++) {
       if (str1.charAt(i) != str2.charAt(i)) diff++;
@@ -28,18 +48,12 @@ public class WordLadder {
   }
 
   private static Map<String, List<String>> buildGraph(List<String> wordList) {
-    Map<String, Set<Integer>> wordChars = wordList.stream().collect(
-      Collectors.toMap(
-        word -> word,
-        word -> word.chars().boxed().collect(Collectors.toSet())
-      )
-    );
     Map<String, List<String>> graph = new HashMap<>();
     for (int i = 0; i < wordList.size(); i++) {
       String wordI = wordList.get(i);
       for (int j = i + 1; j < wordList.size(); j++) {
         String wordJ = wordList.get(j);
-        if (oneDiff(wordChars, wordI, wordJ)) {
+        if (oneDiff(wordI, wordJ)) {
           graph.computeIfAbsent(wordI, k -> new ArrayList<>()).add(wordJ);
           graph.computeIfAbsent(wordJ, k -> new ArrayList<>()).add(wordI);
         }
