37f736bc8e024da05ab04860c066563e79a0b0b1
axell-brendow <breno.axel@gmail.com>
2024-09-01 12:07:45 -0300

    Update shared classes

--- java/myjava/shared/ListNode.java
+++ java/myjava/shared/ListNode.java
@@ -30,6 +30,28 @@ public class ListNode {
   }
 
   @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + val;
+    result = prime * result + ((next == null) ? 0 : next.hashCode());
+    return result;
+  }
+
+  // @Override
+  // public boolean equals(Object obj) {
+  //   if (this == obj) return true;
+  //   if (obj == null) return false;
+  //   if (getClass() != obj.getClass()) return false;
+  //   ListNode other = (ListNode) obj;
+  //   if (val != other.val) return false;
+  //   if (next == null) {
+  //     if (other.next != null) return false;
+  //   } else if (!next.equals(other.next)) return false;
+  //   return true;
+  // }
+
+  @Override
   public boolean equals(Object obj) {
     if (obj == null) return false;
     return toString().equals(obj.toString());
--- java/myjava/shared/TreeNode.java
+++ java/myjava/shared/TreeNode.java
@@ -19,22 +19,28 @@ public class TreeNode {
   }
 
   @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + val;
+    result = prime * result + ((left == null) ? 0 : left.hashCode());
+    result = prime * result + ((right == null) ? 0 : right.hashCode());
+    return result;
+  }
+
+  @Override
   public boolean equals(Object obj) {
     if (this == obj) return true;
     if (obj == null) return false;
     if (getClass() != obj.getClass()) return false;
-
     TreeNode other = (TreeNode) obj;
     if (val != other.val) return false;
-
     if (left == null) {
       if (other.left != null) return false;
     } else if (!left.equals(other.left)) return false;
-
     if (right == null) {
       if (other.right != null) return false;
     } else if (!right.equals(other.right)) return false;
-
     return true;
   }
 
