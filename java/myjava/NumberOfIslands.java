887aea0d30aa883924363590d3b6afaa84b3bd20
axell-brendow <breno.axel@gmail.com>
2024-09-03 16:56:11 -0300

    Adding rank to union-find

--- java/myjava/NumberOfIslands.java
+++ java/myjava/NumberOfIslands.java
@@ -1,6 +1,7 @@
 package myjava;
 
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.*;
 
 /*-
 https://leetcode.com/problems/number-of-islands/description/
@@ -39,36 +40,46 @@ grid[i][j] is '0' or '1'.
 
 public class NumberOfIslands {
   public static int find(int[] parent, int index) {
-    while (parent[index] != index) index = parent[index];
+    while (parent[index] != index) index = parent[index] = parent[parent[index]];
     return index;
   }
 
   public static void union(
-    int[] parent, int[] islands, int nlines, int ncols, int currI, int currJ, int prevI, int prevJ
+    int[] parent, int[] rank, int[] islands, int nlines, int ncols, int currI, int currJ, int prevI, int prevJ
   ) {
     int currIndex = ncols * currI + currJ;
     int prevIndex = ncols * prevI + prevJ;
     int currGroupLeader = find(parent, currIndex);
     int prevGroupLeader = find(parent, prevIndex);
     if (currGroupLeader != prevGroupLeader) {
-      parent[currGroupLeader] = prevGroupLeader;
+      if (rank[prevGroupLeader] >= rank[currGroupLeader]) {
+        parent[currGroupLeader] = prevGroupLeader;
+        rank[prevGroupLeader] += rank[currGroupLeader];
+        rank[currGroupLeader] = 1;
+      } else {
+        parent[prevGroupLeader] = currGroupLeader;
+        rank[currGroupLeader] += rank[prevGroupLeader];
+        rank[prevGroupLeader] = 1;
+      }
       islands[0]--;
     }
   }
 
   public static int numIslands(char[][] grid) {
     int nlines = grid.length, ncols = grid[0].length;
-    int[] parent = new int[nlines * ncols], islands = new int[1];
+    int[] parent = new int[nlines * ncols], rank = new int[nlines * ncols];
+    Arrays.fill(rank, 1);
+    int[] islands = new int[1];
     IntStream.range(0, parent.length).forEach(i -> parent[i] = i);
     for (int i = 0; i < nlines; i++) {
       for (int j = 0; j < ncols; j++) {
         if (grid[i][j] == '1') {
           islands[0]++;
           if (j - 1 >= 0 && grid[i][j - 1] == '1') {
-            union(parent, islands, nlines, ncols, i, j, i, j - 1);
+            union(parent, rank, islands, nlines, ncols, i, j, i, j - 1);
           }
           if (i - 1 >= 0 && grid[i - 1][j] == '1') {
-            union(parent, islands, nlines, ncols, i, j, i - 1, j);
+            union(parent, rank, islands, nlines, ncols, i, j, i - 1, j);
           }
         }
       }
